14b318274f37663650b8d87b96109efa311e4ded
Eric Zimmerman <saericzimmerman@gmail.com>
2021-12-13 21:41:02 -0500

    modernize sln

--- WxTCmd/Properties/AssemblyInfo.cs
+++ WxTCmd/Properties/AssemblyInfo.cs
@@ -1,17 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("WxTCmd")]
-[assembly: AssemblyDescription("Windows 10 Timeline parser")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Eric Zimmerman 501-313-3778")]
-[assembly: AssemblyProduct("WxTCmd")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("Eric R. Zimmerman 501-313-3778")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,16 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e9a2cc9a-ef42-4133-8e0d-c14d22c2b16d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
